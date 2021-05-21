24de7095ba5e351875cad1d718f248b6dbffa99f
ADMIN <ADMIN@praveen>
2021-05-21 13:45:10 -0500

    first commit

---
+++ Gmail/src/test/java/MavenDemo/MavenTest1.java
@@ -0,0 +1,33 @@
+package MavenDemo;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MavenTest1 
+{
+	@Test 
+	public void testPrint1()
+	{
+		System.out.println("testPrint1");	
+	}
+	@Test 
+	public void testPrint2()
+	{
+		System.out.println("testPrint2");	
+	}
+	@Test 
+	public void testPrint3()
+	{
+		System.out.println("testPrint3");	
+	}
+	@Test 
+	public void testPrint4()
+	{
+		System.out.println("testPrint4");	
+	}
+	@Test 
+	public void testPrint5()
+	{
+		System.out.println("testPrint5");	
+		//Assert.fail();
+	}
+}
