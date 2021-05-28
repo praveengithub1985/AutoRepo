2531399308176bd7279ad99fc08e0f32255c1e91
ADMIN <ADMIN@praveen>
2021-05-28 10:38:59 -0500

    ffff

--- ParametersFromJenkins/src/test/java/Yahoo/ExtentReportsDemo.java
+++ ParametersFromJenkins/src/test/java/Yahoo/ExtentReportsDemo.java
@@ -5,17 +5,22 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class ExtentReportsDemo 
 {
 	@Test 
 	public void extent()
 	{
-		//Create object for ExtentReports class
-		ExtentReports report=new ExtentReports();
-		//Start the test case execution
-		//Log the status
-		//End test case exection
-		//Flush
+		ExtentHtmlReporter reporter=new ExtentHtmlReporter("./Reports/FirstReport.html");
+		ExtentReports extent=new ExtentReports();
+		extent.attachReporter(reporter);
+		ExtentTest logger=extent.createTest("Yahoo login test");
+		logger.log(Status.INFO, "Logging in to Yahoo");
+		logger.log(Status.PASS, "Verificaiton Done");
+		extent.flush();		
+		System.out.println("Extent Report Generated");
 	}
 }
